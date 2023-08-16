ca3552531b9ebbbaaa287569da971200705503c1
reza-ak <reeza.dev@gmail.com>
2023-08-16 13:58:05 +0430

    Install and add fortify package and change config (make the password stronger, use username for login)

---
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use Laravel\Fortify\Rules\Password;
+
+trait PasswordValidationRules
+{
+    /**
+     * Get the validation rules used to validate passwords.
+     *
+     * @return array<int, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    protected function passwordRules(): array
+    {
+        return [
+            'required',
+            'string',
+            'confirmed',
+            (new Password)->requireNumeric()
+                ->requireUppercase()
+                ->requireSpecialCharacter(),
+        ];
+    }
+}
